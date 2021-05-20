69815c78e5eb6fcc5ed638c853eea5900de3e9d8
Gustavo Bento de Paula <gustavob.paula@gmail.com>
2021-05-19 23:48:03 -0300

    feat: 🎸 remove breadcrumbs on Home

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,6 +1,14 @@
+import { useStore } from 'hooks'
+import { useEffect } from 'react'
 import { Default as Template } from 'templates'
 
 function Home() {
+  const { setBreadcrumbs } = useStore()
+
+  useEffect(() => {
+    setBreadcrumbs([])
+  }, [setBreadcrumbs])
+
   return <Template />
 }
 
